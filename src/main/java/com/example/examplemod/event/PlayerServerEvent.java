25021fe9908a547ee8c58c215b9676f1058faa24
Flandre923 <1198934671@qq.com>
2024-08-04 11:22:51 +0800

    aff player attack use lighting item create lighting

--- src/main/java/com/example/examplemod/event/PlayerServerEvent.java
+++ src/main/java/com/example/examplemod/event/PlayerServerEvent.java
@@ -1,22 +1,28 @@
 package com.example.examplemod.event;
 
+import com.example.examplemod.item.ModItem;
 import com.example.examplemod.item.enchantment.ModEnchantmentHelper;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.animal.Chicken;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.entity.living.LivingUseTotemEvent;
+import net.neoforged.neoforge.event.entity.player.AttackEntityEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
-import org.apache.logging.log4j.core.jmx.Server;
 
 import java.util.List;
 
@@ -43,6 +49,31 @@ public class PlayerServerEvent {
 
             }
         }
+    }
+
+
+
+    @SubscribeEvent
+    public static void onEntityAttack(AttackEntityEvent event){
+        Player player = event.getEntity();
+        Entity target = event.getTarget();
+        Level world = player.level();
+        if(player.isHolding(ModItem.LIGHTNING_ITEM.get()) && !world.isClientSide){
+            BlockPos blockPos = target.blockPosition();
+            LightningBolt lightningEntity = EntityType.LIGHTNING_BOLT.create(target.level());
+            if (lightningEntity != null) {
+                lightningEntity.moveTo(Vec3.atBottomCenterOf(blockPos));
+                lightningEntity.setCause(player instanceof ServerPlayer ? (ServerPlayer) player : null );
+                target.level().addFreshEntity(lightningEntity);
+                SoundEvent soundEvent = SoundEvents.LIGHTNING_BOLT_IMPACT;
+                player.playSound(soundEvent, 5, 1.0F);
+            }
+        }
+
+        if (target instanceof Chicken && !world.isClientSide){
+            player.sendSystemMessage(Component.literal("哎呦你干嘛"));
+        }
+
     }
 
 
